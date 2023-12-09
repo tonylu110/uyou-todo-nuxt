d2049bded8ee29b5ffd2ad3ee8363fd2b9fe56ec
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-12-09 18:23:54 +0800

    init repo

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useHead({
+  title: 'uyou ToDo',
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/icon.png' }
+  ]
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const links = [{
+  label: 'Home',
+  icon: 'i-heroicons-home',
+  to: '/'
+}]
+</script>
+
+<template>
+  <div class="flex">
+    <div class="bg-black/5 flex-1 flex justify-between flex-col h-screen">
+      <UVerticalNavigation :links="links"/>
+      <UVerticalNavigation :links="[{
+        label: 'Settings',
+        icon: 'i-ph-gear-fine-bold',
+        to: '/settings'
+      }]"/>
+    </div>
+    <div class="w-[80vw]">
+      <slot/>
+    </div>
+  </div>
+</template>
